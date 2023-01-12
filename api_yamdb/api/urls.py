46ca1e8ac4b73b258f6d228f84c976aad505393b
Danil Treskov <treskovdanilv@gmail.com>
2023-01-12 19:03:20 +0500

    register router for Title, Category, Genre viewsets

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet, ReviewViewSet
+from .views import (CommentViewSet, ReviewViewSet, CategoryViewSet,
+                    GenreViewSet, TitleViewSet)
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
+router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
